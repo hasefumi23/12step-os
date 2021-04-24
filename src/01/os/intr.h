7e9bf171c7b95a54ed53ca0125e9146956d803f3
hasefumi23 <hasefumi23@gmail.com>
2021-04-24 23:49:13 +0900

    7 th step implement all

---
+++ src/01/os/intr.h
@@ -0,0 +1,11 @@
+#ifndef _INTR_H_INCLUDED_
+#define _INTR_H_INCLUDED_
+
+/* ソフトウェア割込みベクタの定義 */
+#define SOFTVEC_TYPE_NUM 3 // ソフトウェア割込みベクタの種別の個数
+
+#define SOFTVEC_TYPE_SOFTERR 0 // ソフトウェアエラー
+#define SOFTVEC_TYPE_SYSCALL 1 // システムコール
+#define SOFTVEC_TYPE_SERINTR 2 // シリアル割込み
+
+#endif
